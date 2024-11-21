cf374a96008f6a1e288d7c79d7497032be6b1634
gamman00 <gamman228@naver.com>
2024-11-22 02:30:52 +0900

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,3 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get autoremove -y && sudo apt-get autoclean -y && sudo apt-get clean -y
+sudo cp libapriltag.so /opt/ros/melodic/lib/libapriltag.so
